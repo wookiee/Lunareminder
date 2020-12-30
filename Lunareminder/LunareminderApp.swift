a0f4f5ca9f5c71827a4ae2438ade7f54b5273296
Mikey Ward <mw@mikey.io>
2020-12-30 18:21:50 -0500

    Initial Commit

---
+++ Lunareminder/LunareminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunareminderApp.swift
+//  Lunareminder
+//
+//  Created by Mikey Ward on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct LunareminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
